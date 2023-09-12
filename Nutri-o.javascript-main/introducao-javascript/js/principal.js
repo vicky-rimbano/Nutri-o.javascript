dc730093a1b5de3cc660ac1fea5edba82401d08c
vicky-rimbano <130366712+vicky-rimbano@users.noreply.github.com>
2023-09-12 12:27:20 -0300

    Add files via upload

---
+++ Nutri-o.javascript-main/introducao-javascript/js/principal.js
@@ -0,0 +1,26 @@
+//console.log("O arquivo separado esta funcionando")
+
+//-------------Daqui para baixo começa o codigo 
+
+
+//ateração do titulo
+console.log(document);
+//cria a variavel titulo e guarda o valor da classe
+let titulo = document.querySelector(".titulo");
+//console.log(titulo);
+//guarda um novo valor na variavel titulo
+titulo.textContent = "Fake Natty Nutrição";
+
+//alteração da tabela
+
+let paciente = document.querySelector("#primeiro-paciente");
+//console.log(paciente);
+
+let tdPeso = paciente.querySelector(".info-peso");
+let tdAltura = paciente.querySelector(".info-altura");
+
+let peso = tdPeso.textContent;
+let altura = tdAltura.textContent;
+
+let imc = peso / (altura * altura);
+console.log(imc)
